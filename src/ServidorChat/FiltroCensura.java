7d9136192a96511baceaac236a046e9ea866afcd
HolaPetrova <olga.petrova@colegiomiralmonte.es>
2024-02-06 16:48:21 +0100

    first commit

---
+++ src/ServidorChat/FiltroCensura.java
@@ -0,0 +1,33 @@
+package ServidorChat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FiltroCensura {
+    private List<String> palabrasProhibidas;
+
+    public FiltroCensura() {
+        palabrasProhibidas = new ArrayList<>();
+        // Agrega las palabras prohibidas a la lista
+        palabrasProhibidas.add("nazi");
+        palabrasProhibidas.add("PerroS");
+        palabrasProhibidas.add("sexo");
+        palabrasProhibidas.add("LuisRebollo");
+        // Agrega mÃ¡s palabras segÃºn sea necesario
+    }
+
+    public String filtrarMensaje(String mensaje) {
+        for (String palabraProhibida : palabrasProhibidas) {
+            mensaje = reemplazarConAsteriscos(mensaje, palabraProhibida);
+        }
+        return mensaje;
+    }
+
+    private String reemplazarConAsteriscos(String mensaje, String palabraProhibida) {
+        StringBuilder asteriscos = new StringBuilder();
+        for (int i = 0; i < palabraProhibida.length(); i++) {
+            asteriscos.append("*");
+        }
+        return mensaje.replaceAll("\\b" + palabraProhibida + "\\b", asteriscos.toString());
+    }
+}
